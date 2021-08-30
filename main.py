43552d7ae8853f45ca3a45312e7cace656affeca
Ia Kurnia <iakurnia26@gmail.com>
2021-08-31 04:24:15 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+print("Hello World")
+try:
+    r = requests.get('https://goog le.com')
+    print(r.status_code)
+    if r.status_code == 200:
+        print("berhasil masuk")
+        print(r.text)
+    else:
+        print("gagal request")
+except :
+    print("gagal load request")
+

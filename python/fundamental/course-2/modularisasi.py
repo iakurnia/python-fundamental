3f72065903663dc2cea6bd5334f24d2b5195836f
Ia Kurnia <iakurnia26@gmail.com>
2021-12-08 06:24:11 +0700

    modify pip 8 format

--- python/fundamental/course-2/modularisasi.py
+++ python/fundamental/course-2/modularisasi.py
@@ -2,9 +2,9 @@
 Program menghitung Segitiga
 Luas Segitiga = alas x tinggi /2
 """
-print("---"*10)
+print("---" * 10)
 print('#1.Modular')
-print("---"*10)
+print("---" * 10)
 print('Menghitung Luas Segitiga tanpa Modular')
 alas = 5
 tinggi = 10
@@ -12,9 +12,12 @@ luas = alas * tinggi / 2
 print(f'Alas : {alas} x Tinggi : {tinggi} Luasnya = {luas}')
 
 print('\nMenghitung Luas Segitiga dengan Modular')
-def luasSegitiga(alas,tinggi):
-    luas = alas * tinggi / 2
-    print(f'Alas : {alas} \nTinggi : {tinggi}')
-    return luas;
 
-print(f'Luasnya = {luasSegitiga(10,20)}')
+
+def luasSegitiga(param1, param2):
+    print(f'Alas : {param1} \nTinggi : {param2}')
+    luassegitiga = param1 * param2 / 2
+    return luassegitiga
+
+
+print(f'Luasnya = {luasSegitiga(10, 20)}')
--- python/fundamental/course-2/modularisasiCustom.py
+++ python/fundamental/course-2/modularisasiCustom.py
@@ -1,14 +1,15 @@
 """
 Program Modular dengan type directory berbeda (public in java)
 """
-from segitiga.luas import luasSegitiga,info as info_segitiga
-from persegipanjang.luas import luasPersegiPanjang,info as info_persegipanjang
-print("---"*10)
+from segitiga.luas import luasSegitiga, info as info_segitiga
+from persegipanjang.luas import luasPersegiPanjang, info as info_persegipanjang
+
+print("---" * 10)
 print('#1.Modular Custom')
-print("---"*10)
+print("---" * 10)
 
 print(info_segitiga())
-print(f'Luas Segitinya : {luasSegitiga(10,5)}')
+print(f'Luas Segitinya : {luasSegitiga(10, 5)}')
 
 print(info_persegipanjang())
-print(f'Luas Segitinya : {luasPersegiPanjang(10,5)}')
+print(f'Luas Segitinya : {luasPersegiPanjang(10, 5)}')
